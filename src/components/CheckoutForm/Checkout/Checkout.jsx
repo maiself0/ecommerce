3ce94ebbf9cec9e58838751baaa28e0db0e6ce46
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2021-05-24 19:59:42 +0300

    +: checkout => adressform

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -12,6 +12,7 @@ const steps = ['Shipping address', 'Payment details'];
 const Checkout = ({ cart }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({ })
   const classes = useStyles();
 
   useEffect(()=> {
@@ -25,8 +26,22 @@ const Checkout = ({ cart }) => {
 
       }
     }
+
     generateToken();
   }, [cart]);
+
+
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
+
+
+  const next = (data) => {
+    setShippingData(data);
+
+    nextStep();
+  }
+
+  
 
   const Confirmation = () => (
     <div>
@@ -35,8 +50,8 @@ const Checkout = ({ cart }) => {
   );
 
   const Form = () => activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} />
-    : <PaymentForm />
+    ? <AddressForm checkoutToken={checkoutToken} next={next} />
+    : <PaymentForm shippingData={shippingData} />
 
 
   return (
--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ELementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from 
 
 const PaymentForm = () => {
   return (
